9aa0a029cebeb41bca94201ea1fcb61f9394cca7
shubham awasthi <shubham001kv@gmail.com>
2026-06-07 16:13:55 +0530

    Completed Day 2 Tip Calculator

---
+++ Day02-Tip-Calculator/main.py
@@ -0,0 +1,9 @@
+print("welcome to the shubham calculator")
+bill = (int(input("enter the amount of the bill$ ")))
+tip = (int(input("what amount of tip do you want to give 20 30 40?")))
+people = (int(input("how many people to split the bill? ")))
+bill_with_tip = tip/100*bill+bill
+div = bill/people
+new = div + bill_with_tip
+this = (round("every one is to pay is", new))
+print(this)
